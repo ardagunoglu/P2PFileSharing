a75d8f90773d302aa013b97100411001f56f13b5
Arda Gunoglu <ardagunoglu@gmail.com>
2025-01-07 02:32:39 +0300

    hash first implementation

--- src/main/p2p/view/components/FoundFilesPanel.java
+++ src/main/p2p/view/components/FoundFilesPanel.java
@@ -3,14 +3,16 @@ package main.p2p.view.components;
 import main.p2p.model.Peer;
 
 import javax.swing.*;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class FoundFilesPanel extends AbstractListPanel {
 
+    private Map<String, Map.Entry<String, String>> fileHashMap;
+
     public FoundFilesPanel() {
         super("Found files");
+        fileHashMap = new HashMap<>();
     }
 
     @Override
@@ -27,27 +29,36 @@ public class FoundFilesPanel extends AbstractListPanel {
         DefaultListModel<String> model = new DefaultListModel<>();
         Map<String, Integer> fileNameCounts = new HashMap<>();
 
+        fileHashMap.clear();
+
         for (Map.Entry<String, Map.Entry<String, String>> entry : files.entrySet()) {
-            String relativePath = entry.getKey();
-            String fileName = entry.getValue().getKey();
-            String hash = entry.getValue().getValue();
+            String filePath = entry.getKey();
+            String fileName = filePath.substring(filePath.lastIndexOf("/") + 1);
+            String fileHash = entry.getValue().getValue();
+
+            fileHashMap.put(filePath, entry.getValue());
 
             fileNameCounts.put(fileName, fileNameCounts.getOrDefault(fileName, 0) + 1);
-            int count = fileNameCounts.get(fileName);
+        }
 
-            String fileDisplay = count > 1
-                ? fileName + " (" + count + ")"
-                : fileName;
+        for (Map.Entry<String, Integer> entry : fileNameCounts.entrySet()) {
+            String fileName = entry.getKey();
+            int count = entry.getValue();
 
-            model.addElement(fileDisplay + relativePath);
+            String displayName = count > 1 ? fileName + " (" + count + ")" : fileName;
+            model.addElement(displayName);
         }
 
         getList().setModel(model);
     }
 
-
     public void clearList() {
         DefaultListModel<String> model = (DefaultListModel<String>) getList().getModel();
         model.clear();
+        fileHashMap.clear();
+    }
+
+    public Map<String, Map.Entry<String, String>> getFileHashMap() {
+        return fileHashMap;
     }
 }
